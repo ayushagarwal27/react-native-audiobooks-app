e35d1c47a61b9515c10207008010869ef80cf48e
ayush <ayush@ayushs-MacBook-Air-2.local>
2025-05-01 01:21:36 +0530

    books-flat-list

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import books from "./src/dummy-books";
 import BookListItem from "./src/components/BookListItem";
 
 export default function App() {
   return (
-    <View className="flex-1  justify-center bg-slate-800 p-4">
+    <View className="flex-1  justify-center bg-slate-800 p-4 pt-24">
       {/* Book Rows */}
-      <BookListItem book={books[0]} />
-      <BookListItem book={books[1]} />
-      <BookListItem book={books[2]} />
+      <FlatList
+        data={books}
+        contentContainerClassName="gap-4"
+        renderItem={({ item }) => <BookListItem book={item} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
