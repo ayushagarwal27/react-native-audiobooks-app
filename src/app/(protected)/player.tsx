a287cb3f229a755dfe7f04b7bf63ef258432d3a1
ayush <ayush@ayushs-MacBook-Air-2.local>
2025-05-05 15:06:46 +0530

    add player screen ui

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -1,11 +1,17 @@
-import { View, Text, Pressable } from "react-native";
+import { Text, Pressable, Image, View } from "react-native";
 import React from "react";
 import Entypo from "@expo/vector-icons/Entypo";
 import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import dummyBooks from "@/dummy-books";
+import { Ionicons } from "@expo/vector-icons";
+import PlaybackBar from "@/components/PlaybackBar";
 
 export default function PlayerScreen() {
+  const book = dummyBooks[0];
+
   return (
-    <View className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 bg-gray-900 p-4 py-10 gap-4">
       <Pressable
         onPress={() => {
           router.back();
@@ -14,7 +20,23 @@ export default function PlayerScreen() {
       >
         <Entypo name="chevron-down" size={24} color="white" />
       </Pressable>
-      <Text className="text-white">PlayerScreen</Text>
-    </View>
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="w-[95%] aspect-square rounded-[30px] self-center"
+      />
+      <View className="gap-8 flex-1 justify-end">
+        <Text className="text-white text-2xl font-bold text-center">
+          {book.title}
+        </Text>
+        <PlaybackBar value={0.5} />
+        <View className="flex-row items-center justify-between">
+          <Ionicons name="play-skip-back" size={24} color="white" />
+          <Ionicons name="play-back" size={24} color="white" />
+          <Ionicons name="play" size={50} color="white" />
+          <Ionicons name="play-forward" size={24} color="white" />
+          <Ionicons name="play-skip-forward" size={24} color="white" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
