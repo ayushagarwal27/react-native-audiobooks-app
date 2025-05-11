5d428b161863f731a8bdb363f5962140a92682aa
Ayush <ayushagarwal.dev@gmail.com>
2025-05-11 18:25:37 +0530

    connect clerk with supabase

--- src/app/(protected)/(tabs)/discover.tsx
+++ src/app/(protected)/(tabs)/discover.tsx
@@ -1,11 +1,12 @@
 import { Text, ActivityIndicator } from "react-native";
 import React from "react";
-import { supabase } from "@/lib/supabase";
+import { useSupabase } from "@/lib/supabase";
 import { useQuery } from "@tanstack/react-query";
 import { FlatList } from "react-native";
 import BookListItem from "@/components/BookListItem";
 
 export default function DiscoverScreen() {
+  const supabase = useSupabase();
   const { data, error, isLoading } = useQuery({
     queryKey: ["books"],
     queryFn: () => supabase.from("books").select("*").throwOnError(),
